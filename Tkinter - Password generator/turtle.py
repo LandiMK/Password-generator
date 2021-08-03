1a6746e5ff97960a540666fe9433a17965844571
Lander Iturregi <39025198+LandiMK@users.noreply.github.com>
2021-08-03 20:49:15 +0200

    Add files via upload

---
+++ Tkinter - Password generator/turtle.py
@@ -0,0 +1,14 @@
+# tkinter modules for GUI
+import tkinter as tk
+from tkinter import Button
+from tkinter import Entry
+from tkinter import messagebox
+
+# Modules for password creation
+import secrets
+import string
+
+window = tk.Tk()
+window.title("Password generator")
+window.geometry(300 x 500)
+window.mainloop()
